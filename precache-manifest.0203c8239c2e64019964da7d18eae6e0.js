98498ecd720f7146308e19879808539a1efb409e
Julio García Pérez <julio.garciaperez@telefonica.com>
2019-05-20 20:37:24 +0200

    Updates

---
+++ precache-manifest.0203c8239c2e64019964da7d18eae6e0.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "19797b07b8e42316e1448efb9e2e3ca2",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9db1e8f5a49e39467891",
+    "url": "/static/css/2.5dd60c9c.chunk.css"
+  },
+  {
+    "revision": "85d1042b97598844b438",
+    "url": "/static/css/main.d498bfb3.chunk.css"
+  },
+  {
+    "revision": "9db1e8f5a49e39467891",
+    "url": "/static/js/2.45b3a97c.chunk.js"
+  },
+  {
+    "revision": "85d1042b97598844b438",
+    "url": "/static/js/main.30a5f7be.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/static/media/fontawesome-webfont.fee66e71.woff"
+  }
+]);
